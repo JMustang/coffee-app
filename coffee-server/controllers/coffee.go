2363a0b871c4aec40de09bb25ce31fbfa6f9819f
Junior <eecfredes@gmail.com>
2025-04-12 16:25:58 -0300

    :rocket: Controllers. get all coffees

--- coffee-server/controllers/coffee.go
+++ coffee-server/controllers/coffee.go
@@ -1,1 +1,20 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/JMustang/coffee-app/helpers"
+	"github.com/JMustang/coffee-app/services"
+)
+
+var coffee services.Coffee
+
+func GetAllCoffees(w http.ResponseWriter, r *http.Request) {
+
+	all, err := coffee.GetAllCoffees()
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+		return
+	}
+	helpers.WriteJSON(w, http.StatusOK, helpers.Envelope{"coffees": all})
+}

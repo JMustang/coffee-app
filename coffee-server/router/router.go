e4a922c7e9d1089030b9e0cf35827e4652f64fec
Junior <eecfredes@gmail.com>
2025-04-13 15:26:44 -0300

    :rocket: Router, create

--- coffee-server/router/router.go
+++ coffee-server/router/router.go
@@ -1,1 +1,27 @@
 package router
+
+import (
+	"net/http"
+
+	"github.com/JMustang/coffee-app/controllers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func Router() http.Handler {
+	router := chi.NewRouter()
+	router.Use(middleware.Recoverer)
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	router.Get("/api/v1/coffees", controllers.GetAllCoffees)
+	router.Post("/api/v1/coffees/coffee", controllers.CreateCoffee)
+	return router
+}
